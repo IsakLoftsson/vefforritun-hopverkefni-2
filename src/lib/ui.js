e59e2f94fa8a387699f069e4b831cd413bb9dec2
Gillimar1998 <124063916+Gillimar1998@users.noreply.github.com>
2023-11-23 19:43:49 +0000

    Update ui.js
    lagaði ui.js til að laga html til að gera css fyrir vöru eftir fyrirmynd

--- src/lib/ui.js
+++ src/lib/ui.js
@@ -233,14 +233,19 @@ export async function renderFrontpage(parentElement) {
  * @returns Element sem inniheldur öll gögn um vöru.
  */
 export function createProduct(product) {
-  const productEl = el('div', { class: 'product-site' },
-    el('h1', { class: 'product-title' }, product.title));
+  const productEl = el('div', { class: 'product-site' },);
   if (product.image) {
     productEl.appendChild(el('img', { class: 'product-image', src: product.image }));
   }
-  productEl.appendChild(el('p', { class: 'category_title' }, `Flokkur: ${product.category_title}`));
-  productEl.appendChild(el('p', { class: 'price' }, `Verð: ${product.price} kr.-`));
-  productEl.appendChild(el('p', { class: 'description' }, product.description));
+  const productInfo =   el('div', { class: 'info-Container' })
+
+  productEl.appendChild(productInfo);
+
+  productInfo.appendChild(el('h1', { class: 'product-title' }, product.title))
+  
+  productInfo.appendChild(el('p', { class: 'category_title' }, `Flokkur: ${product.category_title}`));
+  productInfo.appendChild(el('p', { class: 'price' }, `Verð: ${product.price} kr.-`));
+  productInfo.appendChild(el('p', { class: 'description' }, product.description));
   return productEl;
 }
 
